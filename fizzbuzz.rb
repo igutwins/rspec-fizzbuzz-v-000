b631e99f96942250cca2dc72df9624fc2c4cf947
Ian Gutwinski <iangutwinski@gmail.com>
2019-01-20 16:45:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number / 15 = whole number, "Fizzbuzz"
+  elsif number / 3 = whole number, "Fizz"
+  elsif number / 5 = whole number, "Buzz"
+  else nil  
+    
+end 
